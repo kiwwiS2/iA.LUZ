1536ed5ec50c95012a229040af5c232f3c8098eb
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-07-30 12:28:16 -0300

    main.js

--- main.js
+++ main.js
@@ -5,12 +5,36 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 
-//aqui eu posso fazer um comentário 
-
-//nada nestas linhas são de programação, apenas para ajudar a comunidade :)
-
 
 const perguntas = [
+    {
+        enunciado = "Quais são um dos maiores motivos da desigualdade de gênero??",
+        alternativas: [
+            {
+                texto: "A desigualdade de gẽnero pode acontecer por três motivos,estereótipo de gênero, assédio sexual nos ambientes acadêmicos e divisão do trabalho, incluindo a maternidade.",
+                afirmação: "afirmação"
+            },
+            {
+                texto:" Normalmente isso ocorre por assédios sexuais, esteriótipo de gênero e divisão de trabalho. A desigualdade pode ser subdividida em três partes, social, econômica e politíca, tendo como consequência um mundo desigual.",
+                afirmação:"afirmação"
+            }
+        ]
+    },
+    
+    {
+        enunciado = "Na sua opinião, considerando a cultura contemporânea do 'fazer menos é fazer mais', a prática da castração de animais deve ser vista como: uma medida necessária para controlar a superpopulação e promover a saúde dos animais; ou como uma intervenção desnecessária que pode interferir nos processos naturais de reprodução?",
+        alternativas: [
+            {
+                texto: "A prática da castração de animais deve ser vista como uma medida necessária para controlar a superpopulação e promover a saúde dos animais, alinhando-se com a ideia de que menos animais significa mais qualidade de vida para cada um deles.",
+                afirmação: "afirmação"
+            },
+            {
+                texto:"A castração de animais pode ser vista como uma intervenção desnecessária que interfere nos processos naturais de reprodução, desconsiderando que os ecossistemas têm capacidade de ajustar suas populações naturalmente.",
+                afirmação:"afirmação"
+            }
+        ]
+    }, 
+   
     {
         enunciado = "Na sua opinião, considerando a cultura contemporânea do 'fazer menos é fazer mais', a prática da castração de animais deve ser vista como: uma medida necessária para controlar a superpopulação e promover a saúde dos animais; ou como uma intervenção desnecessária que pode interferir nos processos naturais de reprodução?",
         alternativas: [
@@ -24,7 +48,18 @@ const perguntas = [
             }
         ]
     },
-    {
-        enunciado: ""
-    }
 ]
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(atual);
+     if (atual >= ṕerguntas[length]) {
+        mostraResultado ();
+        return;
+     }
+     perguntaAtual = perguntas[atual];
+     caixaPerguntas.textContent = perguntaAtual.enunciado;
+     
